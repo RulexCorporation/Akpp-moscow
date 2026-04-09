637f3bda72d9d770a001cdb317d10a7a0e73431b
Rulex-PO31 <kirillleskavec@gmail.com>
2026-04-09 13:18:10 +0300

    aded yandex metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { BRANDS } from "@/constants/branding";
 import { Suspense } from "react";
-import Template from "./template";
+import Script from "next/script"; // Импортируем компонент для скриптов
 
 export default function RootLayout({
   children,
@@ -12,8 +9,41 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru" className="scroll-smooth">
+      <head>
+        {/* Яндекс.Метрика: вставляем через dangerouslySetInnerHTML */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=108466083", "ym");
+
+              ym(108466083, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                ecommerce:"dataLayer"
+              });
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/108466083"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+      </head>
       <body className="antialiased">
         <Suspense fallback={<div>Загрузка...</div>}>{children}</Suspense>
+
+        {/* Твой существующий JSON-LD */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
